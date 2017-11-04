d78618e2d919cfeab814530f017f9106b1b27cc1
unknown <mattheusjuca@gmail.com>
2017-11-04 19:11:36 -0200

    Implement Data Process Algorithm

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var port = new SerialPort('COM3', {
   baudRate: 9600
 });
 
-var dataBase;
+var dataBase = '';
+var dataArray = new Array(100);
+var arrayIndex = 0;
+var counter = 0;
 
 port.on('error', function(err) {
   console.log('Error: ', err.message);
@@ -14,19 +17,37 @@ port.on('error', function(err) {
 port.on('readable', function () {
   data = port.read();
   data = data.toString('ascii');
-  dataBase = dataBase + data;
-  //console.log(dataBase);
-  setInterval(publish, 3000);
+  processData(data);
 });
 
+function processData(data) {
+  for (var i=0; i < data.length; i++) {
+    if (data[i] === '\n') {
+      counter++;
+    }
+    dataBase = dataBase + data[i];
+    if (counter === 3) {
+      dataArray[arrayIndex] = dataBase;
+      arrayIndex++;
+      counter = 0;
+      dataBase = '';
+      if (arrayIndex === 100) {
+        arrayIndex = 0;
+        //console.log(dataArray);
+        publish();
+      }
+    }
+  }
+}
+
 function publish() {
-  console.log("published");
+  //console.log("published");
   pubnub = new PubNub({
     publishKey: 'pub-c-d3780be2-91fd-466e-9297-c2fe4c633c75',
     subscribeKey: 'sub-c-6566ac86-c19c-11e7-83f0-6e80f1f24680'
   });
   var data = { 
-    'dataBase': dataBase,
+    'data': dataArray
   };
   function publishDataBase() {
     var publishConfig = {
