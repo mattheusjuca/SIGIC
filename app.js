44279f21b1c41bd85b45a3ac12aac7e541c40eb4
unknown <mattheusjuca@gmail.com>
2017-11-05 22:28:04 -0200

    Sending data to PubNub in JSON format

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ var port = new SerialPort('COM4', {
   baudRate: 9600
 });
 
-var dataArrayLength = 100;
-var dataBase = '';
-var dataArray = new Array(dataArrayLength);
-var arrayIndex = 0;
-var counter = 0;
+var measure = '';
+var flag = false;
 
 port.on('error', function(err) {
   console.log('Error: ', err.message);
@@ -24,32 +21,37 @@ port.on('readable', function () {
 function processData(data) {
   for (var i=0; i < data.length; i++) {
     if (data[i] === '\n') {
-      counter++;
+      flag = true;
     }
-    dataBase = dataBase + data[i];
-    if (counter === 3) {
-      dataArray[arrayIndex] = dataBase;
-      arrayIndex++;
-      console.log(arrayIndex);
-      counter = 0;
-      dataBase = '';
-      if (arrayIndex === dataArrayLength) {
-        arrayIndex = 0;
-        console.log(dataArray);
-        publish();
+    measure = measure + data[i];
+    if (flag) {
+      measure = spliter(measure);
+      console.log("Data:", measure);
+      if(measure.length === 3) {
+        publish(measure);
       }
+      flag = false;
+      measure = '';
     }
   }
 }
 
-function publish() {
-  //console.log("published");
+function spliter(measure) {
+  measure = measure.split("T: ").toString().split(" H: ").toString().split(" C: ").toString().substr(1).split("\r\n");
+  measure = measure[0].split(",");
+  return measure;
+}
+
+function publish(measure) {
+  console.log("published");
   pubnub = new PubNub({
     publishKey: 'pub-c-d3780be2-91fd-466e-9297-c2fe4c633c75',
     subscribeKey: 'sub-c-6566ac86-c19c-11e7-83f0-6e80f1f24680'
   });
-  var data = { 
-    'data': dataArray
+  var data = {
+    'temperature': measure[0],
+    'humidity': measure[1],
+    'co': measure[2]
   };
   function publishDataBase() {
     var publishConfig = {
