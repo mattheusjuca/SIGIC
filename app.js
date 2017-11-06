c535d5c9159da0ed8d1bf18ab883020828a0d0fc
unknown <mattheusjuca@gmail.com>
2017-11-05 22:38:19 -0200

    Publish data on PubNub working

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var port = new SerialPort('COM4', {
   baudRate: 9600
 });
 
+var pubnub = new PubNub({
+  publishKey: 'pub-c-d3780be2-91fd-466e-9297-c2fe4c633c75',
+  subscribeKey: 'sub-c-6566ac86-c19c-11e7-83f0-6e80f1f24680'
+});
+
 var measure = '';
 var flag = false;
 
@@ -26,7 +31,7 @@ function processData(data) {
     measure = measure + data[i];
     if (flag) {
       measure = spliter(measure);
-      console.log("Data:", measure);
+      //console.log("Data:", measure);
       if(measure.length === 3) {
         publish(measure);
       }
@@ -43,23 +48,21 @@ function spliter(measure) {
 }
 
 function publish(measure) {
-  console.log("published");
-  pubnub = new PubNub({
-    publishKey: 'pub-c-d3780be2-91fd-466e-9297-c2fe4c633c75',
-    subscribeKey: 'sub-c-6566ac86-c19c-11e7-83f0-6e80f1f24680'
-  });
+  //console.log("published");
   var data = {
-    'temperature': measure[0],
-    'humidity': measure[1],
-    'co': measure[2]
+    'ambient_temperature': Number(measure[0]),
+    'humidity': Number(measure[1]),
+    'co': Number(measure[2])
   };
-  function publishDataBase() {
-    var publishConfig = {
-        channel : "SIGIC-DataBase",
-        message : data
-    }
-    pubnub.publish(publishConfig, function(status, response) {
-        console.log(status, response);
-    })
+  publishData(data);
+}
+
+function publishData(data) {
+  var publishConfig = {
+      channel : "SIGIC-SensorData",
+      message : data
   }
+  pubnub.publish(publishConfig, function(status, response) {
+      console.log(status, response);
+  })
 }
